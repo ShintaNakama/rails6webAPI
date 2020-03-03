e3920566e329ac189ed2e2f209a3ee66144e1eb2
ShintaNakama <shintanakama77@gmail.com>
2020-03-03 23:21:06 +0900

    github action test3

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,6 +1,6 @@
 class HelloController < ApplicationController
   def index
-    res = "helloworld！yes!!!!!!!!!!!!!!!!!!"
+    res = "helloworld！yes!!!!!!!!!!!!!!!!!!77777777"
     render json: res
   end
 end
